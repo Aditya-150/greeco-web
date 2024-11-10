edc95f4ef52ac4d4a959cc90703239d6c5ae00c0
Aditya Prasad Mohanty <adityaprasadmohanty150@gmail.com>
2024-11-10 17:50:32 +0530

    image tag update + git ignore

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -1,12 +1,13 @@
+/* eslint-disable @next/next/no-img-element */
 "use client";
 import { motion } from "framer-motion";
-import Image from "next/image";
+// import Image from "next/image";
 
 export default function Hero() {
   return (
     <div>
       <motion.div>Hero</motion.div>
-      <Image src="/header_image.webp" width={480} height={480} alt="hero image"/>
+      <img alt="hero image" src="/header_image.webp" />
     </div>
   );
 }
